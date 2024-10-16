2541baa3c9445efe7682748b957e8f5179c20e04
Коммерческие курсы <comcuru@teach.local>
2024-10-16 14:41:56 +0300

    initial commit

---
+++ Sport_Inventar/Sport_Inventar/Classes/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Sport_Inventar.Classes
+{
+    class Manager
+    {
+        public Data.User CurrentUser { get; set; }
+        public static Frame MainFrame { get; set; }
+    }
+}
---
+++ Sport_Inventar/Sport_Inventar/Pages/LoginPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sport_Inventar.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StringBuilder errors = new StringBuilder();
+                if (string.IsNullOrEmpty(LoginTexBox.Text))
+                {
+                    errors.AppendLine("заполните логин");
+                }
+                if (string.IsNullOrEmpty(PasswordBox.Password))
+                {
+                    errors.AppendLine("заполните пароль");
+                }
+                if(errors.Length > 0)
+                {
+                    MessageBox.Show(errors.ToString(), "", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+
+                //Data.text1Entities.GetContext().User.Any(d => d.UserLogin == LoginTexBox.Text && d.UserPassword == PasswordBox.Password)
+                //if()
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
